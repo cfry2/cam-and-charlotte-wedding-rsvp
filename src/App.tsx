218f481f1e6f0f31e19509198c5e3f3328ab10e3
Cameron Fry <crfry123@gmail.com>
2022-09-18 11:34:59 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import styled from "@emotion/styled";
+import queryString from "query-string";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { initializeApp } from "firebase/app";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import { getDatabase, ref, child, get, set } from "firebase/database";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+
+const Heading = styled.div({
+  fontFamily: "Roboto",
+  fontSize: "40px",
+});
+
+const {
+  VITE_apiKey,
+  VITE_authDomain,
+  VITE_databaseURL,
+  VITE_projectId,
+  VITE_storageBucket,
+  VITE_messagingSenderId,
+  VITE_appId,
+  VITE_measurementId,
+} = import.meta.env;
+console.log(import.meta);
+const firebaseConfig = {
+  apiKey: VITE_apiKey,
+  authDomain: VITE_authDomain,
+  databaseURL: VITE_databaseURL,
+  projectId: VITE_projectId,
+  storageBucket: VITE_storageBucket,
+  messagingSenderId: VITE_messagingSenderId,
+  appId: VITE_appId,
+  measurementId: VITE_measurementId,
+};
+
+initializeApp(firebaseConfig);
+
+function App() {
+  const { user } = queryString.parse(location.search);
+
+  const [form, setForm] = useState({
+    isLoading: true,
+    rsvp: false,
+    dietries: "",
+    hasSubmitted: false,
+  });
+
+  const { isLoading, rsvp, dietries, hasSubmitted } = form;
+
+  useEffect(() => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `invites/${user}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const { dietries, rsvp } = snapshot.val();
+          setForm({ ...form, isLoading: false, dietries, rsvp });
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        setForm({ ...form, isLoading: false });
+        console.error(error);
+      });
+  }, []);
+
+  const handleSubmit = () => {
+    const db = getDatabase();
+    set(ref(db, `invites/${user}`), {
+      rsvp,
+      dietries,
+    });
+    handleSubmitToggle();
+  };
+
+  const handleSubmitToggle = async () => {
+    setForm({ ...form, isLoading: true });
+    const dbRef = ref(getDatabase());
+    const snapshot = await get(child(dbRef, `invites/${user}`));
+    const { hasSubmitted } = snapshot.val();
+    await set(child(dbRef, `invites/${user}/hasSubmitted`), !hasSubmitted);
+    setForm({ ...form, hasSubmitted: !hasSubmitted, isLoading: false });
+  };
+
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        {hasSubmitted ? (
+          <>
+            <Heading>Thanks {user} for RSVPing to our wedding!</Heading>
+            <Button onClick={handleSubmitToggle} variant="contained">
+              Make a change
+            </Button>
+          </>
+        ) : (
+          <>
+            <Heading>RSVP for {user}</Heading>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={rsvp}
+                    onChange={() => setForm({ ...form, rsvp: !rsvp })}
+                  />
+                }
+                label="Coming"
+              />
+              <TextField
+                id="outlined-basic"
+                margin="dense"
+                label="Any dietry requirements?"
+                variant="outlined"
+                value={dietries}
+                onChange={(e) => setForm({ ...form, dietries: e.target.value })}
+              />
+              <Button onClick={handleSubmit} variant="contained">
+                Submit
+              </Button>
+            </FormGroup>
+          </>
+        )}
+      </Container>
+
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+  );
+}
+
+export default App;
